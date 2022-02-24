8ef645e6a42bcc6aff7746b568ea45aa6d74ad0c
Calvin Freese <calvin.p.freese@gmail.com>
2022-02-23 21:24:13 -0600

    broke out locations into a data file - cleaned up randomizer so that suggestion is never the same location twice

--- src/pages/Locations/Locations.js
+++ src/pages/Locations/Locations.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
+
+import  locations  from '../../data/locations.json';
 
 import { Button } from '../../components/Button/Button';
 import { VerticalCard } from "../../components/Cards/VerticalCard";
+import { ContentBubble } from "../../components/ContentBubble/ContentBubble";
 
 const FlexColumn = styled.div`
     display: flex;
@@ -17,100 +20,74 @@ const FlexRow = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
+    flex-wrap: wrap;
     align-items: center;
     padding: 0px;
     color: #E0EAF0;
 `
 
+const Content = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 175px;`
+
 const MarginBox = styled(FlexColumn)`
     margin: 20px 0;
 `
 export const Locations = () => {
-    const locations = [
-        {
-            key: 1, 
-            name: "Asylum",
-            content: {
-                area: "Large", 
-                floors: "2", 
-                rooms: "111",
-            }
-        },
-        {
-            key: 2, 
-            name: "Bleasedale Farmhouse", 
-            content: {
-                area: "Small", 
-                floors: "3", 
-                rooms: "16"
-            }
-        },
-        {
-            key: 3, 
-            name: "Brownstone Highschool", 
-            content: {
-                area: "Medium", 
-                floors: "2", 
-                rooms: "58"
-            }
-        },
-        {
-            key: 4, 
-            name: "Edgefield Street House",
-            content: {
-                area: "Small", 
-                floors: "3", 
-                rooms: "16"
-            }
-
-        },
-    ];
 
     const [randomizedLocation, setRandomizedLocation] = useState()
-    const [counter, setCounter] = useState(0);
-    const [previousLocations, setPreviousLocations] = useState([]);
-    const [previousSuggestion, setPreviousSuggestion ] = useState('');
 
     const handleRandomize = () => {
         const randomLocation = Math.floor(Math.random() * locations.length)
-        setRandomizedLocation(locations[randomLocation])
-        
-    }
-
-    useEffect(() => {
-        console.log(previousLocations);
-     
-        if(counter > 0 ) {
-            const locationArray = []
-
-            setPreviousSuggestion(randomizedLocation);
-
-            if(previousLocations.length > 0) {
-                locationArray.push(previousLocations);
-            } 
-            
-            locationArray.unshift(previousSuggestion);
-            const flatLocationArray = locationArray.flat().slice(0,5);
-            setPreviousLocations(flatLocationArray)
-
+        if(locations[randomLocation] !== randomizedLocation) {
+            setRandomizedLocation(locations[randomLocation])
+        } else {
+            handleRandomize();
         }
-
-    }, [counter])
-    console.log(locations)
+    }
 
     return (
         <FlexColumn>LOCATION PAGE!
             <MarginBox>
                 {
-                    randomizedLocation?.name ? <VerticalCard title={randomizedLocation.name} content={randomizedLocation.content} /> : <VerticalCard title="?" content={{area: "?", floors: "?", rooms: "?"}} />
+                    randomizedLocation?.name ? (
+                        <VerticalCard title={randomizedLocation.name}>                             
+                            <Content>
+                                <p>Area: </p>
+                                <ContentBubble>{randomizedLocation.content.area}</ContentBubble>
+                            </Content>
+                            <Content>
+                                <p>Floors: </p>
+                                <ContentBubble>{randomizedLocation.content.floors}</ContentBubble>
+                            </Content>
+                            <Content>
+                                <p>Rooms:</p>
+                                <ContentBubble>{randomizedLocation.content.rooms}</ContentBubble>
+                            </Content>
+                        </VerticalCard>
+                    ) : <VerticalCard title="?" content={{area: "?", floors: "?", rooms: "?"}} />
                 }
-                
                 <Button onClick={() => handleRandomize()}>Draw a location card</Button>
             </MarginBox>
             <MarginBox>
                 <FlexRow>
                     {locations &&
-                        locations.map(location => <VerticalCard key={location.key} title={location.name} content={location.content}/>)
+                        locations.map(location => <VerticalCard key={location.key} title={location.name}>
+                            <Content>
+                                <p>Area: </p>
+                                <ContentBubble>{location.content.area}</ContentBubble>
+                            </Content>
+                            <Content>
+                                <p>Floors: </p>
+                                <ContentBubble>{location.content.floors}</ContentBubble>
+                            </Content>
+                            <Content>
+                                <p>Rooms:</p>
+                                <ContentBubble>{location.content.rooms}</ContentBubble>
+                            </Content>
+                        </VerticalCard>)
                     }
                 </FlexRow>
             </MarginBox>
