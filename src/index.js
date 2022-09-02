7fea6e62e9b3f11ab8a1e2fc4518f808248c1585
Calvin Freese <calvin.p.freese@gmail.com>
2022-09-02 12:46:36 -0500

    updated to base hashrouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 
-const APP_ENV = process.env.NODE_ENV;
-const baseUrl = APP_ENV === 'development' ? '/' : 'http://calvin.freese.github.io/phasmophobia-randomizer'
-
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={baseUrl}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
